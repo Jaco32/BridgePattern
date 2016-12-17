2e6a30b34634b05ce636e190635213cd20c745a1
Jaco32 <jaczesku@gmail.com>
2016-12-17 19:36:28 +0100

    Smart pointer used

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
+#include <memory>
+#include <utility>
 #include "Opakowanie.h"
 #include "Przedmiot.h"
 
 int main()
 {
-	Przedmiot* dlugopis = new Dlugopis(new Male(), 10, 10, 0.7, 0.7);
-	Przedmiot* atlas = new Ksiazka(new Duze(), 1000, 15, 2.5, 28);
-	Przedmiot* adidasy = new Buty(new KopertaBabelkowa(), 550, 20, 7, 5);
+	unique_ptr<Opakowanie> male(new Male());
+	unique_ptr<Przedmiot> dlugopis(new Dlugopis(std::move(male), 10, 10, 0.7, 0.7));
+	
+	unique_ptr<Opakowanie> duze(new Duze());
+	unique_ptr<Przedmiot> atlas(new Ksiazka(std::move(duze), 1000, 15, 2.5, 28));
+	
+	unique_ptr<Opakowanie> kopertababelkowa(new KopertaBabelkowa());
+	unique_ptr<Przedmiot> adidasy(new Buty(std::move(kopertababelkowa), 550, 20, 7, 5));
 	
 	dlugopis->spakuj();
 	atlas->spakuj();
 	adidasy->spakuj();
-	
-	delete dlugopis;
-	delete atlas;
-	delete adidasy;
 	
 	return 0;
 }
